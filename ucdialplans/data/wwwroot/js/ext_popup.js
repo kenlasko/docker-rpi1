b5374dea352f21cb7b2cdddc2b2f1d0d64fc60d8
kenlasko <ken.lasko@gmail.com>
2025-04-19 12:53:00 -0400

    Adding instructions for SOPS/age

--- ucdialplans/data/wwwroot/js/ext_popup.js
+++
@@ -1,1 +0,0 @@
-﻿function popUp(URL){var day = new Date();var id=day.getTime();eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=0,location=0,directories=0;statusbar=0,menubar=0,dependent=0,resizable=0,width=500,height=300,left = 200,top = 200');");}
