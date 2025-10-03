4cd628a570485a7407a5b65e8140affe606ab244
Ken Lasko <44060527+kenlasko@users.noreply.github.com>
2025-10-02 20:47:20 -0400

    Update omnibackup.sh

--- omnibackup.sh
+++ omnibackup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+echo "$(date +%F_%T) Backing up Omni etcd database..."
 ETCDCTL_API=3 etcdctl snapshot save /docker/omni/snapshot.db
 day=$(date +%A)
 dayofmonth=$(date +%-d)
 echo "$(date +%F_%T) Backing up omni.asc..."
 sudo cp -f /docker/omni/omni.asc /mnt/omni-backup/
-echo "$(date +%F_%T) Backing up Omni etcd database..."
+echo "$(date +%F_%T) Zipping Omni etcd database..."
 sudo zip -r /mnt/omni-backup/etcdbackup-$day.zip /docker/omni/
 if [ "$dayofmonth" -eq 1 ]; then echo "Creating monthly backup..."; cp /mnt/omni-backup/etcdbackup-$day.zip /mnt/omni-backup/etcdbackup-monthly-$(date +%m).zip; fi
 case $dayofmonth in 7|14|21|28) echo "Creating weekly backup..."; cp /mnt/omni-backup/etcdbackup-$day.zip /mnt/omni-backup/etcdbackup-weekly-$dayofmonth.zip; ;; *) ;; esac
